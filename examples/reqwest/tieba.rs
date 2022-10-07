450c92d14059fa66d13df0b5e8d29a78c28d3171
himawari <1156544355@qq.com>
2022-10-07 21:11:56 +0800

    ✨ 添加图片

--- examples/reqwest/tieba.rs
+++ examples/reqwest/tieba.rs
@@ -1,5 +1,13 @@
 use reqwest;
 use scraper::{Html, Selector};
+use anyhow::{anyhow, Result};
+use futures::stream::StreamExt;
+use std::{borrow::Cow, path::{Path, PathBuf}};
+use structopt::StructOpt;
+use tokio::io::AsyncBufReadExt;
+use tokio::io::BufReader;
+use tokio::{fs::OpenOptions, io::AsyncWriteExt};
+use std::fs;
 
 const TIEBA_URL: &str = "https://tieba.baidu.com/p/6105274031";
 
@@ -19,7 +27,7 @@ const TIEBA_URL: &str = "https://tieba.baidu.com/p/6105274031";
 // }
 #[tokio::main]
 async fn main()  -> Result<(),reqwest::Error> {
-
+    let mut paths: Vec<String> = Vec::new();
     // HTML
     let resp = reqwest::get(TIEBA_URL).await?;
     //println!("Body:{:#?}",resp.text().await?);
@@ -28,7 +36,54 @@ async fn main()  -> Result<(),reqwest::Error> {
     let doc = Html::parse_fragment(&body);
     let selector = Selector::parse(".BDE_Image").unwrap();
     for el in doc.select(&selector) {
-        println!("{}",el.value().attr("src").unwrap());
+        // println!("{}",el.value().attr("src").unwrap());
+        paths.push(el.value().attr("src").unwrap().to_string());
+    }
+    let length = paths.len();
+    let fetches = futures::stream::iter(paths.into_iter().enumerate().map(|(index, url)| {
+        async move {
+            let mut response  = get(&url).await?;
+            let filename = basename(&url, '/');
+            println!("[{:6}/{}] {}", index, length, filename);
+            save(filename.as_ref(), &mut response).await?;
+            Ok::<(), Box<dyn std::error::Error>>(())
+        }
+    }))
+    .buffered(10)
+    .collect::<Vec<_>>();
+    fetches.await;
+
+    Ok(())
+}
+
+fn basename(path: &str, sep: char) -> Cow<str> {
+    let mut pieces = path.rsplit(sep);
+    match pieces.next() {
+        Some(p) => p.into(),
+        None => path.into(),
+    }
+}
+
+async fn get(url: &str) -> Result<reqwest::Response> {
+    reqwest::get(url)
+        .await
+        .map_err(|e| anyhow!("Request url {} error: {}", url, e))
+}
+
+async fn save(filename: &str, response: &mut reqwest::Response) -> Result<()> {
+    let mut options = OpenOptions::new();
+    let mut file = options
+        .append(true)
+        .create(true)
+        .read(true)
+        .open(filename)
+        .await?;
+
+    while let Some(chunk) = &response.chunk().await.expect("Failed") {
+        match file.write_all(&chunk).await {
+            Ok(_) => {}
+            Err(e) => return Err(anyhow!("File {} save error: {}", filename, e)),
+        }
     }
     Ok(())
 }
