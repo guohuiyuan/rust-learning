547d24c43196a3a783b36daa3fc3468fb24be3bd
himawari <1156544355@qq.com>
2022-10-16 14:59:50 +0800

    ✨ 添加sqlite数据库

---
+++ examples/sqlite/transaction.rs
@@ -0,0 +1,34 @@
+use rusqlite::{Connection, Result};
+
+fn main() -> Result<()> {
+    // 打开第一个例子所创建的数据库
+    let mut conn = Connection::open("data/cats.db")?;
+
+    successful_tx(&mut conn)?;
+
+    let res = rolled_back_tx(&mut conn);
+    assert!(res.is_err());
+
+    Ok(())
+}
+
+fn successful_tx(conn: &mut Connection) -> Result<()> {
+    let tx = conn.transaction()?;
+
+    tx.execute("delete from cat_colors", [])?;
+    tx.execute("insert into cat_colors (name) values (?1)", &[&"lavender"])?;
+    tx.execute("insert into cat_colors (name) values (?1)", &[&"blue"])?;
+
+    tx.commit()
+}
+
+fn rolled_back_tx(conn: &mut Connection) -> Result<()> {
+    let tx = conn.transaction()?;
+
+    tx.execute("delete from cat_colors", [])?;
+    tx.execute("insert into cat_colors (name) values (?1)", &[&"lavender"])?;
+    tx.execute("insert into cat_colors (name) values (?1)", &[&"blue"])?;
+    tx.execute("insert into cat_colors (name) values (?1)", &[&"lavender"])?;
+
+    tx.commit()
+}
